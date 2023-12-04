85e385ba88e6f67a7c7278b9b9525ba9bea7972b
Brian Kariu <waiganjo.brian@gmail.com>
2023-12-04 21:04:57 +0300

    ci: change to python terraform
    - Change airbyte build script

---
+++ infra/airbyte/entrypoint.py
@@ -0,0 +1,7 @@
+from python_terraform import Terraform
+
+if __name__ == "__main__":
+    t = Terraform(working_dir='./infra/airbyte/')
+    return_code, stdout, stderr = t.init()
+    return_code, stdout, stderr = t.plan(var_file="prod.tfvars")
+    return_code, stdout, stderr = t.apply()
--- infra/airbyte/variables.tf
+++ infra/airbyte/variables.tf
@@ -1,16 +1,3 @@
 variable "workspace_id" {
   type = string
 }
-
-variable "dataset_id" {
-  type = string
-}
-
-variable "project_id" {
-  type = string
-}
-
-variable "credentials_json_path" {
-  type = string
-}
-
